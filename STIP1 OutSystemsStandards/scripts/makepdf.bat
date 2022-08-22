0469ecae31a18a07f29b0f9ac3b315cae7a0393d
Paul Schmeddes <48135185+pschmeddes@users.noreply.github.com>
2022-08-22 16:34:04 +0200

    Update makepdf.bat

--- STIP1 OutSystemsStandards/scripts/makepdf.bat
+++ STIP1 OutSystemsStandards/scripts/makepdf.bat
@@ -1,4 +1,4 @@
 // pandoc -s -N --variable "geometry=margin=1.2in" --variable mainfont="Palatino" --variable sansfont="Helvetica" --variable monofont="Menlo" --variable fontsize=12pt --variable version=2.0 ../src.title.txt ../src/*.md --include-in-header fancyheaders.tex --toc -o ../build/STIP1OutSystemsDevelopmentStandards.pdf
 
 
-pandoc -s -N --variable "geometry=margin=1.2in" --variable mainfont="Palatino"  --variable sansfont="Helvetica" --variable monofont="Menlo" --variable fontsize=12pt --variable version=2.0 --toc -o ../build/STIP1OutSystemsDevelopmentStandards.pdf ../src/title.txt ../src/0-contents.md ../src/1-Architecture.md ../src/2-Integrations.md ../src/3-NamingConventions.md ../src/4-ProgrammingStandards.md
+pandoc -s -N --variable "geometry=margin=1.2in" --variable mainfont="Palatino"  --variable sansfont="Helvetica" --variable monofont="Menlo" --variable fontsize=12pt --variable version=2.0 --toc -o ../build/STIP1OutSystemsDevelopmentStandards.pdf ../src/title.txt ../src/1-contents.md ../src/2-Architecture.md ../src/3-Integrations.md ../src/4-NamingConventions.md ../src/5-ProgrammingStandards.md ../src/6-StandardResolutionsForArchitectureDashboardFindings.md

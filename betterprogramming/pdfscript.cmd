69cc355da58109fb0d88afaf6a7286ed577c3fc7
Paul <48135185+pschmeddes@users.noreply.github.com>
2022-12-09 17:04:42 +0100

    Update pdfscript.cmd

--- betterprogramming/pdfscript.cmd
+++ betterprogramming/pdfscript.cmd
@@ -1,4 +1,3 @@
-# pandoc version 2.7.3
 mkdir build 
 
-pandoc --pdf-engine=xelatex --template=../templates/eisvogel.latex --highlight-style tango --toc -N -o build/output.pdf src/title.txt src/index.md src/cleancode.md src/finitestatemachine.md src/pragmatic-programmer-tips.md
+pandoc --pdf-engine=xelatex --template=../templates/eisvogel.latex --highlight-style tango --toc -N -o build/output.pdf src/title.txt src/cleancode.md src/finitestatemachine.md src/pragmatic-programmer-tips.md

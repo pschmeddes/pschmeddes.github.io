badd909fd18a7a167f8ac680579bb55558c92eb8
Paul <48135185+pschmeddes@users.noreply.github.com>
2022-12-09 20:44:02 +0100

    Update pdfscript.cmd

--- languages/pdfscript.cmd
+++ languages/pdfscript.cmd
@@ -1,3 +1,3 @@
 mkdir build 
 
-pandoc --pdf-engine=xelatex --template=../templates/eisvogel.latex --highlight-style tango --toc -N -o build/output.pdf swedish/title.txt swedish/vocubalary.md
+pandoc --pdf-engine=xelatex --template=../templates/eisvogel.latex --highlight-style tango --toc -N -o build/swedish.pdf swedish/title.txt swedish/1-guidebook.md swedish/2-guidebook.md swedish/3-guidebook.md swedish/vocubalary.md

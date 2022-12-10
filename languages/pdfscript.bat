0ed91ccafa5ea15b8af86be63cf263d689d02431
Paul <48135185+pschmeddes@users.noreply.github.com>
2022-12-10 19:52:13 +0100

    Update pdfscript.bat

--- languages/pdfscript.bat
+++ languages/pdfscript.bat
@@ -1,5 +1,5 @@
 mkdir build
-copy swedish\*.md build\SwedishCombined.md /b
+copy swedish\*.md /a build\SwedishCombined.md /b /y
 
 
 pandoc -s -N --variable "geometry=margin=1.2in" --variable mainfont="Palatino" --variable sansfont="Helvetica" --variable monofont="Menlo" --variable fontsize=12pt --variable version=2.0 --include-in-header ../templates/fancyheaders.tex --toc -o build/SwedishReference.pdf swedish/title.txt build/SwedishCombined.md

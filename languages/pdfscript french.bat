e5998d5574cb32f3a34c31a46ef1e09ab9c184f2
Paul <48135185+pschmeddes@users.noreply.github.com>
2022-12-25 16:18:35 +0100

    Delete pdfscript french.bat

--- languages/pdfscript french.bat
+++
@@ -1,5 +0,0 @@
-mkdir build
-copy swedish\*.md /a build\SwedishCombined.md /b /y
-
-
-pandoc -s -N --variable "geometry=margin=1.2in" --variable mainfont="Palatino" --variable sansfont="Helvetica" --variable monofont="Menlo" --variable fontsize=12pt --variable version=2.0 --include-in-header ../templates/fancyheaders.tex --toc -o build/French.pdf french/title.txt build/French.md

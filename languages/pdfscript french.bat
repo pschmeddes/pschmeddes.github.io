f2bf69ef26ea07de4968520dcbbce542f376e78e
Paul <48135185+pschmeddes@users.noreply.github.com>
2022-12-25 16:02:33 +0100

    Update pdfscript french.bat

--- languages/pdfscript french.bat
+++ languages/pdfscript french.bat
@@ -2,4 +2,4 @@ mkdir build
 copy swedish\*.md /a build\SwedishCombined.md /b /y
 
 
-pandoc -s -N --variable "geometry=margin=1.2in" --variable mainfont="Palatino" --variable sansfont="Helvetica" --variable monofont="Menlo" --variable fontsize=12pt --variable version=2.0 --include-in-header ../templates/fancyheaders.tex --toc -o build/SwedishReference.pdf swedish/title.txt build/SwedishCombined.md
+pandoc -s -N --variable "geometry=margin=1.2in" --variable mainfont="Palatino" --variable sansfont="Helvetica" --variable monofont="Menlo" --variable fontsize=12pt --variable version=2.0 --include-in-header ../templates/fancyheaders.tex --toc -o build/French.pdf french/title.txt build/French.md

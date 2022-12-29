68dc137f42afcefe4d9bc4651219f6b3c6c18439
Paul Schmeddes <48135185+pschmeddes@users.noreply.github.com>
2022-12-29 14:23:22 +0100

    Update pdfscriptfrench.bat

--- languages/pdfscriptfrench.bat
+++ languages/pdfscriptfrench.bat
@@ -2,4 +2,4 @@ mkdir build
 copy french\*.md /a build\French.md /b /y
 
 
-pandoc -s -N --variable "geometry=margin=1.2in" --variable mainfont="Palatino" --variable sansfont="Helvetica" --variable monofont="Menlo" --variable fontsize=12pt --variable version=2.0 --include-in-header ../templates/fancyheaders.tex --toc -o build/French.pdf french/title.txt build/French.md
+pandoc -s -N --variable "geometry=margin=1.2in" --variable mainfont="Rubik" --variable sansfont="Arial" --variable monofont="Menlo" --variable fontsize=12pt --variable version=2.0 --include-in-header ../templates/fancyheaders.tex --toc -o build/French.pdf french/title.txt build/French.md

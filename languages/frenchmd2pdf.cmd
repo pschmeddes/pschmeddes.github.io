ecbe6172cad46616a414238cce70f6a5e549e455
Paul <48135185+pschmeddes@users.noreply.github.com>
2023-01-01 16:38:13 +0100

    Update frenchmd2pdf.cmd

--- languages/frenchmd2pdf.cmd
+++ languages/frenchmd2pdf.cmd
@@ -1,21 +1,12 @@
 ::===============================================================
-:: This scripts converts a .md (markdown) file to a .pdf
+:: This scripts creates a pdf of all md files in the french directory
 ::
 ::===============================================================
 echo off
-:: Command line parameter check 
-REM IF NOT ERRORLEVEL 1 GOTO Syntax
-:: IF "%~1"=="" GOTO Syntax
-:: IF "%~2"=="" GOTO Syntax
-:: Check sourefile exists
-:: set sourcefile=%~1
-:: if not exist "%sourcefile%" (
-::    echo Cannot find source file %1
-::    goto:EOF
-::)
-
-:: make build directory
+:: make sure the build directory exist
 if not exist build mkdir build
+:: delete previous pdf
+if exist build/frenc.pdf del build/french.pdf
 :: combine to one document
 copy french\*.md /a build\French.md /b /y
 :: Pandoc convert to pdf
@@ -36,21 +27,6 @@ pandoc -s -N ^
    -o build/French.pdf french/title.txt build/French.md
 echo conversion completed
 goto:EOF
-::pandoc build/French.md ^
-    -f gfm ^
-    --include-in-header chapter_break.tex ^
-    --include-in-header inline_code.tex ^
-    --highlight-style pygments.theme ^
-    -V linkcolor:blue ^
-    -V geometry:a4paper ^
-    -V geometry:margin=2cm ^
-    -V mainfont="DejaVu Serif" ^
-    -V monofont="DejaVu Sans Mono" ^
-    --pdf-engine=xelatex ^
-    -o build/French.pdf french/title.txt 
-echo conversion completed
-goto:EOF
 :Syntax 
 ECHO Convert markdown to pdf 
 ECHO Usage: md2pdf [ markdownsourcefile ] [ pdfFileName]
-
